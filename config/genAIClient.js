7003968d4c68c6802b58c4c51d368535113eda42
AkhilKompally <akhilkompally27@gmail.com>
2025-11-04 11:00:56 +0530

    Fixing in Generating images

--- config/genAIClient.js
+++ config/genAIClient.js
@@ -1,7 +1,9 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
 dotenv.config();
 
+const ai = new GoogleGenAI({apiKey : process.env.API_KEY}); 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-export default genAI;
+export default { genAI, ai };
